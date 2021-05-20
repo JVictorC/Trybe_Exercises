3ea2513eb9d251ec0bc1ae00c22c2a9653e0599e
Joao Victor <joaogmavictor@gmail.com>
2021-05-20 18:48:22 -0300

    Concluindo os Exercicios do Bloco 4.2 dia 20/05/2021

---
+++ Exercises/Bloco_4/4.2/Parte9.js
@@ -0,0 +1,10 @@
+/*Utilizando o array criado no exercício anterior imprima o 
+resultado da divisão de cada um dos elementos por 2 .*/
+
+let number = [];
+
+for (let index = 0; index <= 25; index += 1) {
+    number.push(index)
+    console.log(number[index]/2);
+}
+
