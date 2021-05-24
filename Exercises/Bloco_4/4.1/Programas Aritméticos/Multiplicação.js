755225ecd0860b394021ce648a7efcab41786075
Joao Victor <joaogmavictor@gmail.com>
2021-05-24 20:16:42 -0300

    Concluindo as atividades assincronas do dia 24/05/2021 bloco 4.4

--- Exercises/Bloco_4/4.1/Programas Aritméticos/Multiplicação.js
+++ Exercises/Bloco_4/4.1/Programas Aritméticos/Multiplicação.js
@@ -2,3 +2,15 @@ let a = 10;
 let b = 5;
 
 console.log(a * b);
+
+/* Refaturando Bloco 4.4 */
+
+console.log("Fazendo com functions")
+
+
+function mult (a, b){
+   
+    return  a * b;
+
+}
+console.log(mult(10,5))
