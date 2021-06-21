2d7626a2890ca6c69366330f6bf33542cc41d0b6
Joao Victor <joaogmavictor@gmail.com>
2021-06-21 18:50:59 -0300

    Concluindo Função 5

--- Exercises/Bloco_7/7.3/ParteIFunção2.js
+++ Exercises/Bloco_7/7.3/ParteIFunção2.js
@@ -18,6 +18,8 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4, 5], 6), [1, 2, 3, 4, 5])
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
 
 /*
+A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+
 Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
 Verifique se o array passado por parâmetro não sofreu alterações
