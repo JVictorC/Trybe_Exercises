ae6a80f5058034575638c7a05c85d1f94b69793c
Joao Victor <joaogmavictor@gmail.com>
2021-06-21 18:42:21 -0300

    Concluindo Função 3

--- Exercises/Bloco_7/7.3/ParteIFunção2.js
+++ Exercises/Bloco_7/7.3/ParteIFunção2.js
@@ -17,5 +17,10 @@ assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4])
 assert.deepStrictEqual(myRemove([1, 2, 3, 4, 5], 6), [1, 2, 3, 4, 5])
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
 
-
+/*
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+Verifique se o array passado por parâmetro não sofreu alterações
+Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+*/
 
