9daebafb06aab690536da02b255eb15583e541f9
Joao Victor <joaogmavictor@gmail.com>
2021-10-14 17:53:43 -0300

    Alterando Esquemas de Pastas

---
+++ Exercises/Back-End/Bloco_19/19.1/CreateMyFirstTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Filme`.`Filmes`(
+	FilmeId INT NOT NULL AUTO_INCREMENT,
+    Descricao VARCHAR(100) NOT NULL,
+    AnoLancamento INT NOT NULL,
+    Nota INT,
+    
+    PRIMARY KEY(FilmeID)
+)
