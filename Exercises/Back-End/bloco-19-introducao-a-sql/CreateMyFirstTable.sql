51720a13dc843e304ead740145a11e7f61be7023
Joao Victor <joaogmavictor@gmail.com>
2021-10-14 17:54:01 -0300

    Alterando Esquemas de Pastas

--- Exercises/Back-End/bloco-19-introducao-a-sql/CreateMyFirstTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `Filme`.`Filmes`(
-	FilmeId INT NOT NULL AUTO_INCREMENT,
-    Descricao VARCHAR(100) NOT NULL,
-    AnoLancamento INT NOT NULL,
-    Nota INT,
-    
-    PRIMARY KEY(FilmeID)
-)
